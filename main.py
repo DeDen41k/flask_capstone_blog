6e078f1c95972076948141fe87f485baff5ef463
DeDen41k <dedenistoma139@gmail.com>
2024-02-19 17:11:07 +0200

    New commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from forms import CreatePostForm, LoginForm, RegisterForm, CommentForm
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 bcrypt = Bcrypt(app)
@@ -42,7 +42,7 @@ def admin_only(f):
 
 
 # CREATE DATABASE
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI', 'sqlite:///posts.db')
 db = SQLAlchemy(app)
 
 
